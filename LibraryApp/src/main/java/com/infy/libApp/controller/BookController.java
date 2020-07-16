1be663e91516330a11d10f162290866c1fb23feb
Abdul-Aleem029 <45032252+Abdul-Aleem029@users.noreply.github.com>
2020-07-16 14:47:49 +1000

    Update BookController.java

--- LibraryApp/src/main/java/com/infy/libApp/controller/BookController.java
+++ LibraryApp/src/main/java/com/infy/libApp/controller/BookController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.io.File;
 
 import com.infy.libApp.model.Books;
 import com.infy.libApp.service.BookService;
@@ -15,10 +16,15 @@ public class BookController {
 
 	@Autowired
 	BookService service;
+	
+	@Value("${welcome.message}")
+	private String configFilePath;
+	
 
 	@RequestMapping("/")
 	public String welcome(Map<String, Object> model) 
 	{
+		File file = new File(configFilePath);
 		List<Books> books = service.getAllBooks();
 		for (Books books2 : books) {
 			System.out.println(books2.getId());
